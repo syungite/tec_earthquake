aaf090bf2af6f14ab291b923c48839f6c8b4fe38
syungite <earthwithheart@gmail.com>
2024-10-24 16:22:21 +0900

    delete additonal file

--- src/calct.py
+++
@@ -1,4 +0,0 @@
-def main():
-    for i in range(51):
-        output_anomaly_graph = f"../data/anomaly/anomaly_{i}.png"
-        with open(output_anomaly_graph.replace(".png", ".txt"), 'r') as anomaly_file:       
